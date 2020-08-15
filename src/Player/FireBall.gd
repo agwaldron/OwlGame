1e6da87ceaf4c7b353b41b5cb474cbe965bdf7b0
awal <awal_dron@yahoo.com>
2020-08-15 16:06:57 -0600

    Fire ball goes away after collision with wall

--- src/Player/FireBall.gd
+++ src/Player/FireBall.gd
@@ -2,11 +2,11 @@ extends KinematicBody2D
 
 export var SPEED = 1000
 
-var velocity = Vector2(0, 0)
+var velocity = Vector2(SPEED, 0)
 
 func _process(delta):
 	velocity = move_and_slide(velocity)
 
-func _on_HitBox_area_entered(area):
-	print("hit")
+
+func _on_HitBox_body_entered(body):
 	queue_free()
