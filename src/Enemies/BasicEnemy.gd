f3c992984f0a20bee9d0ea6796d15fb77db8de9d
awal <awal_dron@yahoo.com>
2020-08-17 18:05:03 -0600

    Fireball collides with enemies, can set enemy health and enemy disappears when health reaches zero

--- src/Enemies/BasicEnemy.gd
+++ src/Enemies/BasicEnemy.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 onready var stats = $EnemyStats
 
-
+func _ready():
+	stats.health = 2
 
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
--- src/Player/FireBall.gd
+++ src/Player/FireBall.gd
@@ -8,3 +8,9 @@ var velocity = Vector2(SPEED, 0)
 
 func _process(delta):
 	velocity = move_and_slide(velocity)
+
+func _on_HitBox_area_entered(area):
+	queue_free()
+
+func _on_HitBox_body_entered(body):
+	queue_free()
