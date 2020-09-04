7059299858284ee571e932e7479bc43ba41760fc
awal <awal_dron@yahoo.com>
2020-09-04 17:49:09 -0600

    Pilot has health and receives damage. Disapears on health reaching 0. Remains stationary and does not attack

---
+++ src/Enemies/ThePilot/ThePilot.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+onready var stats = $EnemyStats
+
+func _ready():
+	stats.health = 15
+
+func _on_HurtBox_area_entered(area):
+	stats.health -= area.damage
+
+func _on_EnemyStats_no_health():
+	queue_free()
