ab064d7ae971eb543aa8936d383cb33fdf5f3488
awal <awal_dron@yahoo.com>
2020-10-13 12:34:51 -0600

    The Witch: spawns vertical and horizontal daggers for attack. Both follow player around then shoot on timer. Timings and speeds could use some tweaking

--- src/Enemies/TheWitch/Spells/MagicDagger.gd
+++ src/Enemies/TheWitch/Spells/MagicDagger.gd
@@ -18,41 +18,71 @@ var velocity = Vector2.ZERO
 var playerpos
 var target
 var horoffset = 35
-var trackTimer = 500
-var pullbackTimer = 10
+var vertoffset = 35
+var hortracktimer = 500
+var vertracktimer = 250
+var pullbackTimer = 20
 var timer
+var ishor
 
 func _ready():
 	animatedSprite.play("Horizontal")
 	animatedSprite.set_frame(0)
 	animatedSprite.playing = false
 	state = TRACKING
-	timer = trackTimer
+	timer = hortracktimer
 	horHitBox.disabled = false
+	ishor = true
 
 func _process(delta):
 	match state:
 		TRACKING:
-			target = playerpos
-			target.y -= horoffset
-			velocity = position.direction_to(target) * trackspeed
-			velocity.x = 0
-			timer -= (delta * 100)
-			if timer <= 0:
-				state = PULLBACK
-				velocity.y = 0
-				velocity.x = pullbackspeed
-				timer = pullbackTimer
-				animatedSprite.playing = true
+			tracking(delta)
 		PULLBACK:
-			timer -= (delta * 100)
-			if timer <= 0:
-				state = FLYING
-				velocity.x = flyspeed * -1
+			pullBack(delta)
 	velocity = move_and_slide(velocity)
 
 func isVertical():
-	pass
+	animatedSprite.play("Vertical")
+	animatedSprite.set_frame(0)
+	animatedSprite.playing = false
+	horHitBox.disabled = true
+	vertHitBox.disabled = false
+	ishor = false
+	timer = vertracktimer
+
+func tracking(delta):
+	target = playerpos
+	timer -= (delta * 100)
+	if timer <= 0:
+		state = PULLBACK
+		animatedSprite.playing = true
+		timer = pullbackTimer
+		if ishor:
+			velocity.x = pullbackspeed
+			velocity.y = 0
+		else:
+			velocity.x = 0
+			velocity.y = pullbackspeed * -1
+	else:
+		target = playerpos
+		if ishor:
+			target.y -= horoffset
+			velocity = position.direction_to(target) * trackspeed
+			velocity.x = 0
+		else:
+			target.x -= vertoffset
+			velocity = position.direction_to(target) * trackspeed
+			velocity.y = 0
+
+func pullBack(delta):
+	timer -= (delta * 100)
+	if timer <= 0:
+		state = FLYING
+		if ishor:
+			velocity.x = flyspeed * -1
+		else:
+			velocity.y = flyspeed
 
 func updatePlayerLocation(pos):
 	playerpos = pos
--- src/Enemies/TheWitch/TheWith.gd
+++ src/Enemies/TheWitch/TheWith.gd
@@ -1,26 +1,58 @@
 extends KinematicBody2D
 
 const BeePortal = preload("res://src/Enemies/TheWitch/Spells/BeePortal.tscn")
+const MagicDagger = preload("res://src/Enemies/TheWitch/Spells/MagicDagger.tscn")
 
 onready var animatedSpirte = $AnimatedSprite
 onready var stats = $EnemyStats
 
-var beeCoolDown = 50000
-var beeTimer
+var beecooldown = 50000
+var beetimer
+var attackcooldown = 700
+var attacktimer
+var vertdaggerpos = 100
+var hordaggerpos = 900
+var playerpos
 
 func _ready():
 	stats.health = 20
-	beeTimer = beeCoolDown
+	beetimer = beecooldown
+	attacktimer = attackcooldown
 
 func _process(delta):
-	beeTimer -= (delta * 100)
-	if beeTimer <= 0:
-		var beePortal = BeePortal.instance()
-		get_parent().add_child(beePortal)
-		beePortal.global_position = global_position
-		beePortal.global_position.x -= 300
-		beePortal.global_position.y -= 300
-		beeTimer = beeCoolDown
+	beeSpawns(delta)
+	attacktimer -= (delta * 100)
+	if attacktimer <= 0:
+		attack()
+
+func beeSpawns(delta):
+	beetimer -= (delta * 100)
+	if beetimer <= 0:
+		var beeportal = BeePortal.instance()
+		get_parent().add_child(beeportal)
+		beeportal.global_position = global_position
+		beeportal.global_position.x -= 300
+		beeportal.global_position.y -= 300
+		beetimer = beecooldown
+
+func attack():
+	var magicdagger = MagicDagger.instance()
+	get_parent().add_child(magicdagger)
+	magicdagger.playerpos = playerpos
+	magicdagger.global_position.x = hordaggerpos
+	magicdagger.global_position.y = playerpos.y - magicdagger.horoffset
+
+	magicdagger = MagicDagger.instance()
+	get_parent().add_child(magicdagger)
+	magicdagger.playerpos = playerpos
+	magicdagger.isVertical()
+	magicdagger.global_position.x = playerpos.x - magicdagger.vertoffset
+	magicdagger.global_position.y = vertdaggerpos
+
+	attacktimer = attackcooldown
+
+func updatePlayerLocation(pos):
+	playerpos = pos
 
 func _on_HurtBox_area_entered(area):
 	var areaGroups = area.get_groups()
