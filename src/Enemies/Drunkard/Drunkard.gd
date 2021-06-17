3f3134d5501eff2a48a3630f41a6f41ea9b1bd4b
awal <awal_dron@yahoo.com>
2021-06-17 16:09:15 -0600

    Drunkard moves left to right, turns around when hits wall

--- src/Enemies/Drunkard/Drunkard.gd
+++ src/Enemies/Drunkard/Drunkard.gd
@@ -1,9 +1,38 @@
 extends KinematicBody2D
 
+enum{
+	LEFTNOBEER,
+	LEFTBEER,
+	RIGHTNOBEER,
+	RIGHTBEER
+}
+
 onready var animatedSprite = $AnimatedSprite
 onready var hurtBox = $HurtBox/CollisionShape2D
 onready var stats = $EnemyStats
 
-# Called when the node enters the scene tree for the first time.
+var state
+var speed = 275
+var velocity = Vector2.ZERO
+var movingleft
+
 func _ready():
-	pass # Replace with function body.
+	state = LEFTBEER
+	animatedSprite.play("MoveLeftBeer")
+	movingleft = true
+	velocity = Vector2(speed * -1, 0)
+
+func _process(delta):
+	if is_on_wall():
+		turn_around()
+	velocity = move_and_slide(velocity)
+
+func turn_around():
+	if movingleft:
+		animatedSprite.play("MoveRightBeer")
+		movingleft = false
+		velocity = Vector2(speed, 0)
+	else:
+		animatedSprite.play("MoveLeftBeer")
+		movingleft = true
+		velocity = Vector2(speed * -1, 0)
